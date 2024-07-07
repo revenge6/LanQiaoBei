8081145ca4341362c34af90472757f8ca2ebb999
nintoka <uni18236068303@163.com>
2024-07-07 23:17:14 +0800

    插入提交

--- src/shujuku/Insert.java
+++ src/shujuku/Insert.java
@@ -1,13 +1,20 @@
+package shujuku;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-public class DatabaseUtils {
+class DatabaseUtils {
 
     private static final String url = "url";
+    private String[][] fields;
+    // 假设主键列名称是一个字符串
+    private String primaryKeyColumn;
+    public String getPriKey() {
+        return this.primaryKeyColumn;
+    }
 
-    // 函数定义
     public boolean insert(String tableName, String[][] fields) {
         Connection conn = null;
         PreparedStatement pstmt = null;
@@ -64,4 +71,36 @@ public class DatabaseUtils {
             }
         }
     }
+    public void swapRow() {
+        String primaryKey = getPriKey(); // 获取主键值
+
+        // 找到主键所在的行索引
+        int rowIndex = findRow(primaryKey);
+
+        if (rowIndex == -1) {
+            System.out.println("主键值未找到或数组为空");
+            return;
+        }
+
+        // 交换找到的行与第一行的数据
+        swapRows(0, rowIndex);
+    }
+
+    // 根据主键查找对应的行索引
+    private int findRow(String primaryKey,String[][] fields) {
+        for (int i = 0; i < fields.length; i++) {
+            if (fields[i][1].equals(primaryKey)) {
+                return i; // 返回主键所在行的索引
+            }
+        }
+        return -1; // 没有找到主键值，返回-1
+    }
+
+    // 交换数组的两行数据
+    private void swapRows(int row1, int row2) {
+        String[] temp = fields[row1];
+        fields[row1] = fields[row2];
+        fields[row2] = temp;
+    }
 }
+
