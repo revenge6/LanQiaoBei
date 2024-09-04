59fa28d34e2c24a1bcf71566273e9adacb474a66
nintoka <uni18236068303@163.com>
2024-09-04 14:29:22 +0800

    add

--- src/Test.java
+++ src/Test.java
@@ -2,30 +2,38 @@ import shujuku.DataBase;
 import shujuku.GGG;
 import shujuku.GtTest;
 import shujuku.Service;
+import shujuku.MainClass;
 //实践类，测试代码运行结果
-public class Test {
-    public static void main(String[] args) {
-        Service service=new Service("jdbc:sqlite:D:\\java\\idea-workspace\\LanQiaoBei\\test.db");
-//        GtTest one=new GtTest(18,1,98,"gttt");
-        GGG one=new GGG(18,1,98,"gttt");
-//        GtTest two=new GtTest(19,2,60,"hk");
-//        GtTest four=new GtTest(19,1,"32","hk");
-//        GtTest three=new GtTest(20,1,"hk");
+//public class Test {
+//    public static void main(String[] args) {
+//        Service service=new Service("jdbc:sqlite:\\D:\\java\\idea-workspace\\LanQiaoBei\\test.db\\");
+//        GtTest one=new GtTest(18,4,"2022","white");
+//        GGG one=new GGG(18,1,98,"gttt");
+//        GtTest two=new GtTest(19,2, "2021","black");
+        //       GtTest four=new GtTest(23,5,"2020","red");
+//       GtTest three=new GtTest(20,3,"2023","yellow");
 //        Xuliehua xuliehua=new Xuliehua();
-        service.Add(one,"id");
+//        service.Add(one);
 //        service.Add(two);
 //        service.Update(four);
 //        service.Add(three);
 //        service.Add(four);
 //        service.Add(xuliehua);
 //        service.Delete(two);
-//        m();
-    }
-    public static void m(){
-        DataBase.DeleteTable("shujuku$GtTest");
+        //       m();
+
+
+
+
+
+//        public static void m(){
+//        DataBase.DeleteTable("shujuku$GtTest");
 //        DataBase.DeleteTable("Xuliehua");
-        DataBase.DeleteTable("Map");
-        DataBase.DeleteTable("Attribute");
-        System.out.println("删除表成功！");
-    }
-}
+//        DataBase.DeleteTable("Map");
+//        DataBase.DeleteTable("Attribute");
+//        System.out.println("删除表成功！");
+ //
+        // }
+
+
+
