acfcf1c81bba545f7158ec9c0a94d3c99a36b8ef
anna20040130 <15226029750@163.com>
2024-09-12 17:35:51 +0800

    jar

---
+++ src/shujuku/GGG.java
@@ -0,0 +1,15 @@
+package shujuku;
+
+//以这个类为测试类，将类的实例对象添加到数据库中
+public class GGG{
+    int age;
+    int id;
+    int grade;
+    String name;
+    public GGG(int age, int id,int grade,String name) {
+        this.age = age;
+        this.id = id;
+        this.grade=grade;
+        this.name=name;
+    }
+}
